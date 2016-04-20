1cd39625a0f6776882c62ef0f65df30bd3e6c1ff
HarryStevens <harryjosephstevens@gmail.com>
2016-04-20 10:11:57 +0530

    minor tweaks

--- quoteShare.js
+++ quoteShare.js
@@ -1,77 +1,93 @@
 ( function($) {
 
+		var thisArray = [];
+
 		$.fn.quoteShare = function(options) {
 
-			var settings = $.extend({
-				backgroundColor : '#55acee',
-				birdColor : '#ffffff',
-				minLength : 1,
-				maxLength : 114,
-				shareIcon : '<i class="fa fa-twitter-square" style="margin-top:-2px" aria-hidden="true"></i>',
-				size : 24
-			}, options);
+			return this.each(function() {
 
-			var url = window.location.href;
+				var unique = $(this).context.offsetTop;
 
-			var hidePopup = {
-				'position' : 'absolute',
-				'top' : '-10000px',
-				'left' : '-10000px'
-			};
-			$('body').append('<div id="qs-popup">');
-			$('#qs-popup').css({
-				'background' : settings.birdColor,
-				'font-size' : settings.size,
-				'height' : (settings.size) - 5,
-				'width' : (settings.size) - 4,
-				'border-radius' : '6px',
-			});
-			$('#qs-popup').append('<a class="qs-sharelink">'+settings.shareIcon+'</a>');
-			$("a.qs-sharelink").css({
-				'color' : settings.backgroundColor,
-				'cursor' : 'pointer',
-				'opacity' : '.9'
-			});
-			$("a.qs-sharelink").hover(function() {
-				$(this).css('opacity', '1');
-			}, function() {
-				$(this).css('opacity', '.9');
-			});
-			$('#qs-popup').css(hidePopup).addClass('qs-popup-hidden').removeClass('qs-popup-shown');
+				var id = 'qs-popup-' + unique;
 
-			var history = [];
+				console.log(id);
 
-			var text = "";
-			this.mouseup(function(e) {
+				//some variables to be used later
+				var history = [];
+				var text = "";
+				var url = window.location.href;
 
-				var showPopup = {
-					'top' : e.pageY - 10,
-					'left' : e.pageX
+				//options
+				var settings = $.extend({
+					backgroundColor : '#55acee',
+					birdColor : '#ffffff',
+					minLength : 1,
+					maxLength : 114,
+					shareIcon : '<i class="fa fa-twitter-square share-icon" aria-hidden="true"></i>',
+					size : 24
+				}, options);
+
+				//css
+				var hidePopup = {
+					'position' : 'absolute',
+					'top' : '-10000px',
+					'left' : '-10000px'
 				};
+				$('body').append('<div id="' + id + '">');
 
-				var selection = window.getSelection();
+				$('#' + id).css({
+					'background' : settings.birdColor,
+					'font-size' : settings.size,
+					'height' : settings.size - ((settings.size) / 4.5),
+					'width' : settings.size - ((settings.size) / 5.5),
+					'border-radius' : (settings.size) / 4.5,
+				});
 
-				var text = selection.toString();
-				var len = text.length;
+				$('#' + id).append('<a class="qs-sharelink qs-sharelink-'+unique+'">' + settings.shareIcon + '</a>');
+				$("a.qs-sharelink-"+unique).css({
+					'color' : settings.backgroundColor,
+					'cursor' : 'pointer',
+				});
+				$('#' + id).css(hidePopup).addClass('qs-popup-hidden').removeClass('qs-popup-shown');
+				$('.share-icon').css('margin-top', -((settings.size) / 12.25));
 
-				history.push(text);
-				var historyLength = history.length;
+				$(this).mouseup(function(e) {
 
-				if (len > settings.minLength && len < settings.maxLength && (text != history[historyLength - 2]) && selection.type == "Range") {
-					$('#qs-popup').css(showPopup).addClass('qs-popup-shown').removeClass('qs-popup-hidden');
-					var shareURL = 'http://twitter.com/share?&amp;text="' + text + '" ' + url;
-					$('a.qs-sharelink').attr('onclick', "popUp=window.open('" + shareURL + "','popupwindow','scrollbars=yes,width=600,height=250');popUp.focus();return false");
-				} else {
-					$('#qs-popup').css(hidePopup).addClass('qs-popup-hidden').removeClass('qs-popup-shown');
-				}
+					var showPopup = {
+						'top' : e.pageY - 10,
+						'left' : e.pageX
+					};
 
-				$('.qs-popup-shown').click(function() {
-					$(this).css(hidePopup).addClass('qs-popup-hidden').removeClass('qs-popup-shown');
+					var selection = window.getSelection();
+
+					var text = selection.toString();
+					var len = text.length;
+
+					history.push(text);
+					var historyLength = history.length;
+
+					if (len > settings.minLength && len < settings.maxLength && (text != history[historyLength - 2]) && selection.type == "Range") {
+						$('#' + id).css(showPopup).addClass('qs-popup-shown').removeClass('qs-popup-hidden');
+						var shareURL = 'http://twitter.com/share?&amp;text="' + text + '" ' + url;
+						$('a.qs-sharelink').attr('onclick', "popUp=window.open('" + shareURL + "','popupwindow','scrollbars=yes,width=600,height=250');popUp.focus();return false");
+					} else {
+						$('#' + id).css(hidePopup).addClass('qs-popup-hidden').removeClass('qs-popup-shown');
+					}
+
+					$('.qs-popup-shown').click(function() {
+						$(this).css(hidePopup).addClass('qs-popup-hidden').removeClass('qs-popup-shown');
+					});
+
+				});
+
+				$('body').mousedown(function() {
+
+					if ($('#' + id).hasClass('qs-popup-shown')) {
+						$('#' + id).css(hidePopup).removeClass('qs-popup-shown').addClass('qs-popup-hidden');
+					}
 				});
 
 			});
-
-			return this;
 
 		};
 
